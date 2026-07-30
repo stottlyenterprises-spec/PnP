6eb3eacd81112c4c553fc80b9f7efb2578b8298f
Master Control <mastercontrol@Gryphs-iMac.local>
2026-07-30 16:22:27 -0400

    Harden account restoration and native layout

--- ios/App/App/MainViewController.swift
+++ ios/App/App/MainViewController.swift
@@ -1,8 +1,29 @@
 import Capacitor
+import UIKit
 
 final class MainViewController: CAPBridgeViewController {
+    private let deedsBackground = UIColor(
+        red: 14.0 / 255.0,
+        green: 32.0 / 255.0,
+        blue: 25.0 / 255.0,
+        alpha: 1
+    )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = deedsBackground
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+
     override func capacitorDidLoad() {
         bridge?.registerPluginInstance(DeedsPrivacyPlugin())
         bridge?.registerPluginInstance(DeedsHealthPlugin())
+        bridge?.webView?.isOpaque = false
+        bridge?.webView?.backgroundColor = deedsBackground
+        bridge?.webView?.scrollView.backgroundColor = deedsBackground
+        bridge?.webView?.scrollView.contentInsetAdjustmentBehavior = .never
     }
 }
